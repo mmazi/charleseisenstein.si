f1e2dbcd16c5bb02b6dc44800a0e2450ae1eb54b
jfticar <jernej.fticar@protonmail.com>
2023-02-16 20:29:38 +0100

    Started work on mobile hamburger menu
    Part 16 Copy 1 should contain the files I began working on

--- js/script.js
+++ js/script.js
@@ -6,4 +6,32 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
-  }
+}
+
+// JS
+// Get the elements by their ids
+var popupBtn = document.getElementById("popup-btn");
+var popupWindow = document.getElementById("popup-window");
+var closeBtn = document.getElementById("close-btn");
+
+// Add an event listener to show the pop-up window when the button is clicked
+popupBtn.addEventListener("click", function() {
+    popupWindow.style.display = "block";
+});
+
+// Add an event listener to hide the pop-up window when the "x" button is clicked
+closeBtn.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+});
+
+// Add a line to hide the close button when the "x" button is clicked
+closeBtn.addEventListener("click", function() {
+  popupWindow.style.display = "none";
+  closeBtn.style.display = "none"; // add this line
+});
+
+// Add a line to show the close button when the pop-up button is clicked
+popupBtn.addEventListener("click", function() {
+  popupWindow.style.display = "block";
+  closeBtn.style.display = "block"; // add this line
+});
